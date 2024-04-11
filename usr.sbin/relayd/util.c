d8d65038e349a5dab802032302094ccbaeaffe9b
Mateusz Piotrowski <0mp@FreeBSD.org>
2024-04-11 13:19:31 +0200

    freebsd-relayd: relayd: util.c: Include sys/param.h instead of sys/types.h
    Obtained from: https://github.com/swills/relayd at 32d3e6d fix mismerge

--- usr.sbin/relayd/util.c
+++ usr.sbin/relayd/util.c
@@ -16,7 +16,11 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
+#ifdef __FreeBSD__
+#include <sys/param.h>
+#else
 #include <sys/types.h>
+#endif
 #include <sys/socket.h>
 #include <sys/time.h>
 
