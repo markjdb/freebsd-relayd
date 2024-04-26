cc381fdfa454a9b24c0a061602f2ef25472d06e2
Mark Johnston <markj@FreeBSD.org>
2024-04-26 14:12:50 -0400

    freebsd-relayd: Fix problems with HCE/PFE/relay desync after reload
    It has been observed that relayd will sometimes exit with an assertion
    failure immediately after its config has been reloaded (i.e., SIGHUP was
    sent or "relayctl reload" was used).  Usually it's because the PFE was
    handling a host status update (IMSG_HOST_STATUS) and failed to find the
    host in question or the check counts aren't matching.
    
    relayd configuration reloads are implemented by the parent, which
    re-reads the configuration file, builds up a new set of data structures
    (hosts, tables, redirects, etc.), and sends copies to each worker.
    The sequence is:
    1. parent -> IMSG_CTL_RESET -> workers, causing workers to free all of
       the existing data structures.
    2. parent -> IMSG_CFG_{TABLE,HOST,...} -> workers, giving state to
       workers.
    3. parent -> IMSG_CFG_DONE -> workers, indicating that it's done with
       the configuration update.
    4. workers -> IMSG_CFG_DONE -> parent, indicating workers are ready to
       start.
    5. parent -> IMSG_CFG_START -> workers, only once all messages in step 4
       have been received by the parent.
    
    The structures sent in step 2 each have a unique ID (modulo 32-bit
    object ID rollover), so a host object ID is only valid in one
    "configuration epoch".  When a reload starts, a new configuration epoch
    begins, and all IDs belonging to the previous epoch become invalid.
    
    The basic problem is that there is no global ordering of IPC messages
    sent between workers and the parent.  Each process in relayd receives
    messages from multiple queues, and these can be interleaved or delayed
    indefinitely.  For instance, IMSG_HOST_STATUS (sent from HCE -> PFE, and
    PFE -> relay) refers to a host by ID, but there is nothing ensuring that
    the status update is handled in the same epoch in which it was sent.
    Such mismatches result in the assertion failures mentioned above.
    
    One solution is simply to remove checks and drop messages if they refer
    to non-existent objects.  However, this might hide bugs and generally
    makes the code harder to reason about.  My solution is to make the
    notion of configuration epoch explicit in relayd worker state, and to
    tag certain messages with the current epoch.  Then workers can decide
    what to do about mismatches.  So:
    
    1. Add a 64-bit epoch counter, incremented in step 1 of the reload
       procedure above.  That is, each worker has a notion of the current
       epoch, incremented when IMSG_CTL_RESET is received.
    2. When the HCE schedules host checks, it stamps the host with the
       current epoch.  When IMSG_HOST_STATUS is set to the PFE and to
       relays, the receiver drops the message if the epoch doesn't match.
       This ensures that old host status messages are correctly ignored.
    3. "script" checks involve a round-trip through the parent process which
       isn't cancelled when the HCE receives IMSG_CTL_RESET.  Thus, imbue
       script state with the current epoch as well.
    4. Ensure that the PFE and HCE actually disable events when CTL_RESET is
       received.  Previously, hce_disable_events() and pfe_disable_events()
       weren't getting called at all.
    5. Make sure that relayctl can't be used to schedule a check while a
       configuration reload is in progress.
    
    This was sufficient to eliminate assertion failures in a stress test
    which spams relayd with SIGHUP and relayctl poll commands.  I suspect
    that my coverage of item 5 above isn't wide enough; that is, other
    relayctl commands (e.g., relayctl table disable) might still cause
    problems.

--- usr.sbin/relayd/relayd.h
+++ usr.sbin/relayd/relayd.h
@@ -162,6 +162,7 @@ struct ctl_status {
 	int		 up;
 	int		 retry_cnt;
 	u_long		 check_cnt;
+	u_int64_t	 config_gen;
 	u_int16_t	 he;
 };
 
@@ -193,6 +194,7 @@ struct ctl_relayfd {
 
 struct ctl_script {
 	objid_t		 host;
+	u_int64_t	 config_gen;
 	int		 retval;
 	struct timeval	 timeout;
 	char		 name[HOST_NAME_MAX+1];
@@ -481,6 +483,7 @@ struct host {
 	u_long			 up_cnt;
 	int			 retry_cnt;
 	int			 idx;
+	u_int64_t		 config_gen;
 	u_int16_t		 he;
 	int			 code;
 	struct ctl_tcp_event	 cte;
@@ -1059,7 +1062,7 @@ enum imsg_type {
 	IMSG_CA_PRIVDEC,
 	IMSG_SESS_PUBLISH,	/* from relay to pfe */
 	IMSG_SESS_UNPUBLISH,
-	IMSG_TLSTICKET_REKEY
+	IMSG_TLSTICKET_REKEY,
 };
 
 enum privsep_procid {
@@ -1206,6 +1209,8 @@ struct relayd {
 
 	struct privsep		*sc_ps;
 	int			 sc_reload;
+	int			 sc_started;
+	u_int64_t		 sc_config_gen;
 };
 
 #define RELAYD_OPT_VERBOSE		0x01
